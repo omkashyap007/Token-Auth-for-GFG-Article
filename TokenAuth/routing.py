a340e1246827274690b13232e909518fc1d83728
omkashyap007 <omkashyapcric@gmail.com>
2022-10-23 15:49:22 +0530

    Initial Commit

---
+++ TokenAuth/routing.py
@@ -0,0 +1,20 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path
+from token_auth_app.consumers import TokenAuthConsumer
+from token_auth_app.middlewares import TokenAuthMiddleWare
+
+application = ProtocolTypeRouter(
+    {
+        "websocket": TokenAuthMiddleWare(
+            AllowedHostsOriginValidator(
+                URLRouter(
+                    [
+                        path("", TokenAuthConsumer.as_asgi())
+                    ]
+                )
+            )
+        )
+    }
+)
---
+++ token_auth_app/consumers.py
@@ -0,0 +1,18 @@
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+class TokenAuthConsumer(AsyncJsonWebsocketConsumer) : 
+    async def connect(self):
+        await self.accept()
+        print(self.scope["user"].username)
+        print(self.scope["user"].email)
+    async def disconnect(self , close_code):
+        ...
+        
+    async def receive_json(self , message):
+        command = message.get("command") 
+        if command == "Say hello !" : 
+            await self.send_json({
+                "command_response": "The command to say hello was recevied " ,
+                "data_string_bacK" : message.get("data_string" , None) 
+            }) 
+        
